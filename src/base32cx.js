78111328f457e80e9fc84364399b4d41b2c50c71
Mr Word <noman.word@protonmail.com>
2020-01-24 13:02:08 -0500

    rm blake2 references (using sha256 only), update deps, standard --fix

--- src/base32cx.js
+++ src/base32cx.js
@@ -1,103 +1,103 @@
-const sha256 = require('js-sha256').sha256;
+const sha256 = require('js-sha256').sha256
 
 const alphabet = '456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-function hex2bytes(hex) {
-    let bytes = [];
-    for (let i = 0; i < hex.length; i += 2) {
-        let _byte = parseInt(hex.substr(i, 2), 16);
-        bytes.push(_byte);
-    }
-    return bytes;
+function hex2bytes (hex) {
+  const bytes = []
+  for (let i = 0; i < hex.length; i += 2) {
+    const _byte = parseInt(hex.substr(i, 2), 16)
+    bytes.push(_byte)
+  }
+  return bytes
 }
 
-function getBit(_byte, index) {
-    return 1 & (_byte >> (7-(index%8)));
+function getBit (_byte, index) {
+  return 1 & (_byte >> (7 - (index % 8)))
 }
 
-function packBit(_byte, index, bit) {
-    return _byte | (0xff & (bit << (7 - index%8)))
+function packBit (_byte, index, bit) {
+  return _byte | (0xff & (bit << (7 - index % 8)))
 }
 
-function encode(bytes) {
-    if( bytes.length > (1024 * 1024) - 1 ) {
-        throw new Error("base32cc encode is not defined for data longer than 2^20-1 bytes.");
+function encode (bytes) {
+  if (bytes.length > (1024 * 1024) - 1) {
+    throw new Error('base32cc encode is not defined for data longer than 2^20-1 bytes.')
+  }
+  const hash = sha256(bytes)
+  const hashbytes = hex2bytes(hash)
+  const bitlen = bytes.length * 8
+  const outbytes = []
+  for (let i = 0; i < bitlen; i++) {
+    // the i'th bit is in the i/8th byte at the i%8th position
+    //  (left to right, most-significant bit first)
+    // it goes into the i/5th char into the (2+(i%5))'th position
+    //  (left to right, most-sigificant bit first)
+    // ie, 01010101 -> ___01010 ___101**
+    const i8 = Math.floor(i / 8)
+    const i5 = Math.floor(i / 5)
+    const bit = getBit(bytes[i8], i % 8)
+    if (outbytes[i5] === undefined) {
+      outbytes[i5] = 0
     }
-    const hash = sha256(bytes);
-    const hashbytes = hex2bytes(hash);
-    const bitlen = bytes.length * 8;
-    let outbytes = [];
-    for (let i = 0; i < bitlen; i++) {
-        // the i'th bit is in the i/8th byte at the i%8th position
-        //  (left to right, most-significant bit first)
-        // it goes into the i/5th char into the (2+(i%5))'th position
-        //  (left to right, most-sigificant bit first)
-        // ie, 01010101 -> ___01010 ___101**
-        let i8 = Math.floor(i/8);
-        let i5 = Math.floor(i/5);
-        let bit = getBit(bytes[i8], i%8)
-        if( outbytes[i5] === undefined ) {
-            outbytes[i5] = 0;
-        }
-        outbytes[i5] = packBit(outbytes[i5], 3 + i%5, bit);
+    outbytes[i5] = packBit(outbytes[i5], 3 + i % 5, bit)
+  }
+  const chars = outbytes.map((x) => alphabet[x])
+  for (let i = 0; i < chars.length; i++) {
+    const ii = i % 256
+    const hashbit = getBit(hashbytes[Math.floor(ii / 8)], ii % 8)
+    if (hashbit == 0) {
+      chars[i] = chars[i].toLowerCase()
     }
-    let chars = outbytes.map((x) => alphabet[x]);
-    for( let i = 0; i < chars.length; i++) {
-        let ii = i % 256;
-        let hashbit = getBit(hashbytes[Math.floor(ii/8)], ii%8);
-        if( hashbit == 0 ) {
-            chars[i] = chars[i].toLowerCase();
-        }
-    }
-    return chars.join('');
+  }
+  return chars.join('')
 }
 
-function decode(str) {
-    if( str.length > ((1024*1024 - 1) * (8/5)) ) {
-        throw new Error("base32cc decode is not defined for strings longer than 1677720 characters (2^20 decoded bytes).");
+function decode (str) {
+  if (str.length > ((1024 * 1024 - 1) * (8 / 5))) {
+    throw new Error('base32cc decode is not defined for strings longer than 1677720 characters (2^20 decoded bytes).')
+  }
+  const rem = str.length % 8
+  if (rem == 1 || rem == 3 || rem == 6) {
+    throw new Error(`Invalid base32cc encoding inferred from length ${str.length}`)
+  }
+  const bitlen = str.length * 5
+  const raw = str.toUpperCase()
+  const outbytes = []
+  for (let i = 0; i < bitlen; i++) {
+    const i8 = Math.floor(i / 8)
+    const i5 = Math.floor(i / 5)
+    const c = raw[i5]
+    const b = alphabet.indexOf(raw[i5])
+    const bit = getBit(b, 3 + i % 5)
+    if (outbytes[i8] === undefined) {
+      outbytes[i8] = 0
     }
-    let rem = str.length % 8;
-    if (rem == 1 || rem == 3 || rem == 6) {
-        throw new Error(`Invalid base32cc encoding inferred from length ${str.length}`)
+    outbytes[i8] = packBit(outbytes[i8], i % 8, bit)
+  }
+  if (str.length % 8 != 0) {
+    outbytes.pop()
+  }
+  const hashbytes = hex2bytes(sha256(outbytes))
+  for (let i = 0; i < str.length; i++) {
+    const ii = i % 256
+    const hashbit = getBit(hashbytes[Math.floor(ii / 8)], ii % 8)
+    const c = str[i]
+    if (hashbit == 0) {
+      if (isNaN(c) && c == c.toUpperCase()) {
+        throw new Error(`Checksum failed, expected character ${str[i]} at position ${i} to be lowercase`)
+      }
+    } else {
+      if (isNaN(c) && c == c.toLowerCase()) {
+        throw new Error(`Checksum failed, expected character ${str[i]} at position ${i} to be uppercase`)
+      }
     }
-    const bitlen = str.length * 5;
-    const raw = str.toUpperCase();
-    let outbytes = [];
-    for( let i = 0; i < bitlen; i++ ) {
-        let i8 = Math.floor(i/8);
-        let i5 = Math.floor(i/5);
-        let c = raw[i5];
-        let b = alphabet.indexOf(raw[i5]);
-        let bit = getBit(b, 3 + i%5);
-        if (outbytes[i8] === undefined) {
-            outbytes[i8] = 0;
-        }
-        outbytes[i8] = packBit(outbytes[i8], i%8, bit);
-    }
-    if (str.length % 8 != 0) {
-        outbytes.pop();
-    }
-    let hashbytes = hex2bytes(sha256(outbytes));
-    for( let i = 0; i < str.length; i++) {
-        let ii = i % 256;
-        let hashbit = getBit(hashbytes[Math.floor(ii/8)], ii%8);
-        let c = str[i];
-        if( hashbit == 0 ) {
-            if( isNaN(c) && c == c.toUpperCase() ) {
-                throw new Error(`Checksum failed, expected character ${str[i]} at position ${i} to be lowercase`);
-            }
-        } else {
-            if( isNaN(c) && c == c.toLowerCase() ) {
-                throw new Error(`Checksum failed, expected character ${str[i]} at position ${i} to be uppercase`);
-            }
-        }
-    }
-    return outbytes;
+  }
+  return outbytes
 }
 
 module.exports = {
-    getBit: getBit
-  , packBit: packBit
-  , encode: encode
-  , decode: decode
+  getBit: getBit,
+  packBit: packBit,
+  encode: encode,
+  decode: decode
 }
--- test/base32cx-test.js
+++ test/base32cx-test.js
@@ -1,108 +1,107 @@
-const want = require('chai').expect;
+const want = require('chai').expect
 
-const b32x = require('../src/base32cx.js');
+const b32x = require('../src/base32cx.js')
 
-function ascii2bytes(a) {
-    return a.split('').map((a)=>{ return a.charCodeAt(0) });
+function ascii2bytes (a) {
+  return a.split('').map((a) => { return a.charCodeAt(0) })
 }
 
-describe('base32cx', ()=>{
-    it('canonical test cases', () => {
-        function pass(str) {
-            console.log(`ascii    : ${str}`);
-            let bytes = ascii2bytes(str);
-            let encoded = b32x.encode(bytes);
-            console.log(`base32cx : ${encoded}`);
-            let decoded = b32x.decode(encoded);
-            want(b32x.decode(
-                    b32x.encode(ascii2bytes(str))))
-                .to.deep.equal(ascii2bytes(str));
+describe('base32cx', () => {
+  it('canonical test cases', () => {
+    function pass (str) {
+      console.log(`ascii    : ${str}`)
+      const bytes = ascii2bytes(str)
+      const encoded = b32x.encode(bytes)
+      console.log(`base32cx : ${encoded}`)
+      const decoded = b32x.decode(encoded)
+      want(b32x.decode(
+        b32x.encode(ascii2bytes(str))))
+        .to.deep.equal(ascii2bytes(str))
+    }
+
+    pass('H')
+    pass('He')
+    pass('Hel')
+    pass('Hell')
+    pass('Hello')
+    pass('Hello!')
+
+    pass('abcdef')
+    pass('\x00abc\x00def')
+
+    pass('000011112222333344445555666677778888')
+    pass('000000001111111122222222333333334444444455555555666666667777777788888888')
+    pass(Array(513).fill('!').join(''))
+
+    // multibase repo test cases
+    pass('Decentralize everything!!')
+    pass('yes mani !')
+    pass('hello world')
+    pass('\x00yes mani !')
+    pass('\x00\x00yes mani !')
+  })
+
+  it('packBit', () => {
+    want(b32x.packBit(0x00, 0, 1)).to.equal(128)
+    want(b32x.packBit(0x00, 7, 1)).to.equal(1)
+  })
+  it('getBit', () => {
+    want(b32x.getBit(0x01, 7)).to.equal(1)
+    want(b32x.getBit(0x01, 6)).to.equal(0)
+    want(b32x.getBit(0x02, 6)).to.equal(1)
+    want(b32x.getBit(128, 0)).to.equal(1)
+    want(b32x.getBit(0, 4)).to.equal(0)
+
+    want(b32x.getBit(['\0'][0], 0)).to.equal(0)
+    want(b32x.getBit([128][0], 0)).to.equal(1)
+  })
+  it('data too long to encode', () => {
+    const oversize = 1024 * 1024
+    const bigBytes = Array(oversize).fill(0)
+    want(() => { b32x.encode(bigBytes) }).to.throw
+    b32x.encode(Array(oversize - 1).fill(0))
+  })
+  it('encode', () => {
+    // 00000001 (00..) -> ___00000 ___001(00)
+    //                      (0->4)   (4->8)
+    want(b32x.encode([0x01, 0, 0, 0, 0])).to.deep.equal('48444444')
+    // 00001001 (00..) -> ___00001 ___001(00)
+    //                      (1->5)   (4->8)
+    want(b32x.encode([0x09, 0, 0, 0, 0])).to.deep.equal('58444444')
+  })
+  it('decode', () => {
+    want(b32x.decode('48444444')).to.deep.equal([1, 0, 0, 0, 0])
+  })
+  it('x == encode(decode(x))', () => {
+    function pass (str) {
+      want(b32x.encode(b32x.decode(str))).to.equal(str)
+    }
+    pass('44')
+    pass('48444444')
+    pass('58444444')
+    pass('5844444')
+    pass('58444')
+    pass('5844')
+    pass('58')
+  })
+  it('x == decode(encode(x))', () => {
+    function pass (bytes) {
+      want(b32x.decode(b32x.encode(bytes))).to.deep.equal(bytes)
+    }
+    pass([0, 0, 0, 0, 0])
+    pass([0, 0, 0, 0, 1])
+    pass([1, 0, 0, 0, 1])
+    const trials = 20
+    const max = 50
+    console.log(`running ${trials * max} tests up to length ${max}, this will take some time...`)
+    for (let trial = 0; trial < trials; trial++) {
+      for (let len = 0; len < max; len++) {
+        const arr = []
+        for (let i = 0; i < 20; i++) {
+          arr.push(Math.floor(256 * Math.random()))
+          pass(arr)
         }
-
-        pass('H');
-        pass('He');
-        pass('Hel');
-        pass('Hell');
-        pass('Hello');
-        pass('Hello!');
-
-        pass('abcdef');
-        pass('\x00abc\x00def');
-
-        pass('000011112222333344445555666677778888');
-        pass('000000001111111122222222333333334444444455555555666666667777777788888888');
-        pass(Array(513).fill('!').join(''));
-
-        // multibase repo test cases
-        pass("Decentralize everything!!");
-        pass("yes mani !");
-        pass("hello world");
-        pass("\x00yes mani !");
-        pass("\x00\x00yes mani !");
-    });
-
-    it('packBit', () => {
-        want(b32x.packBit(0x00, 0, 1)).to.equal(128);
-        want(b32x.packBit(0x00, 7, 1)).to.equal(1);
-    });
-    it('getBit', () => {
-        want(b32x.getBit(0x01, 7)).to.equal(1);
-        want(b32x.getBit(0x01, 6)).to.equal(0);
-        want(b32x.getBit(0x02, 6)).to.equal(1);
-        want(b32x.getBit(128, 0)).to.equal(1);
-        want(b32x.getBit(0, 4)).to.equal(0);
-
-        want(b32x.getBit(['\0'][0], 0)).to.equal(0);
-        want(b32x.getBit([128][0], 0)).to.equal(1);
-    })
-    it('data too long to encode', () => {
-        let oversize = 1024 * 1024;
-        let bigBytes = Array(oversize).fill(0);
-        want(() => { b32x.encode(bigBytes) }).to.throw;;
-        b32x.encode(Array(oversize - 1).fill(0));
-    });
-    it('encode', () => {
-        // 00000001 (00..) -> ___00000 ___001(00)
-        //                      (0->4)   (4->8)
-        want(b32x.encode([0x01, 0, 0, 0, 0])).to.deep.equal('48444444');
-        // 00001001 (00..) -> ___00001 ___001(00)
-        //                      (1->5)   (4->8)
-        want(b32x.encode([0x09, 0, 0, 0, 0])).to.deep.equal('58444444');
-    });
-    it('decode', () => {
-        want(b32x.decode('48444444')).to.deep.equal([1, 0, 0, 0, 0]);
-    });
-    it('x == encode(decode(x))', () => {
-        function pass(str) {
-            want(b32x.encode(b32x.decode(str))).to.equal(str);
-        }
-        pass('44');
-        pass('48444444');
-        pass('58444444');
-        pass('5844444');
-        pass('58444');
-        pass('5844');
-        pass('58');
-    });
-    it('x == decode(encode(x))', () => {
-        function pass(bytes) {
-            want(b32x.decode(b32x.encode(bytes))).to.deep.equal(bytes);
-        }
-        pass([0, 0, 0, 0, 0]);
-        pass([0, 0, 0, 0, 1]);
-        pass([1, 0, 0, 0, 1]);
-        let trials = 5;
-        let max = 10;
-        console.log(`running ${trials*max} tests up to length ${max}, this will take some time...`);
-        for( let trial = 0; trial < trials; trial++ ) {
-            for( let len = 0; len < max; len++ ) {
-                let arr = [];
-                for( let i = 0; i < 20; i++) {
-                    arr.push(Math.floor(256*Math.random()));
-                    pass(arr);
-                }
-            }
-        }
-    }).timeout(0);
-});
-
+      }
+    }
+  }).timeout(0)
+})
